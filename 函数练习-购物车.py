ed693761158b834769b0bcb49287b40ce4cc7a20
Whk-123 <60881457+Whk-123@users.noreply.github.com>
2020-02-12 20:46:20 +0800

    Create 函数练习-购物车.py

---
+++ 函数练习-购物车.py
@@ -0,0 +1,49 @@
+'''
+将商品添加进购物车后验证有没有登录，没有的登录，有则将商品添加进购物车
+'''
+
+def add_ShoppingCart(goods_name):
+    if online == 0:
+        print('您还没有登录，请登录')
+        login_ing()
+    else:
+
+        ShoppingCart.append(goods_name)
+        print("{}已添加进购物车！".format(goods_name))
+        print("你的购物车内有：", ShoppingCart)
+        answer = input('是否继续购物？y/n  \n')
+        if answer == 'y':
+            return shopping()
+
+
+def login(username, password):
+    user = '123'
+    passw = '123'
+
+    if username == user and password == passw:
+        print("登录成功")
+        global online
+        online = 1
+        return add_ShoppingCart(goods_name1)
+
+    else:
+        print('用户名或密码错误！')
+        login_ing()
+
+
+def login_ing():
+
+    username1 = input('请输入用户名：')
+    password1 = input('请输入密码：')
+    return login(username1, password1)
+
+
+def shopping():
+    global goods_name1
+    goods_name1 = input("你要购买什么商品？ \n")
+    add_ShoppingCart(goods_name1)
+
+ShoppingCart=[]
+online = 0
+shopping()
+
